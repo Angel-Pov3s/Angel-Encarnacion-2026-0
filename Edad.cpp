097f0bef3f895cf183b7a14ab88bc93ecc1fbb07
unknown <angel.encarnacion@unas.edu.pe>
2026-01-19 17:13:46 -0500

    v1.1

---
+++ Edad.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+int edad;
+    cout << "Escribe tu edad ";
+    cin >> edad;
+    cout <<"Tienes: "<<edad<<" anios de edad"<<endl; //Yo escribo anios por que en el programa no muestra la letra "ñ"
+    return 0;
+}
+    
---
+++ Multiplicacion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2;
+    float producto;
+
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    producto = num1 * num2;
+
+    cout << "El resultado de la multiplicacion es: " << producto << endl;
+
+    return 0;
+}
